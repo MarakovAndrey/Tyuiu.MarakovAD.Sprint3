8ca8f0ab4d94e4dcaa91f2cd130e2f16c320248e
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-10-27 19:13:37 +0500

    Task4

---
+++ Tyuiu.MarakovAD.Sprint3.Task4.V13.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MarakovAD.Sprint3.Task4.V13.Lib;
+namespace Tyuiu.MarakovAD.Sprint3.Task4.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(-5, 5);
+            double wait = 1.899;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MarakovAD.Sprint3.Task4.V13/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.MarakovAD.Sprint3.Task4.V13.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Мараков А. Д. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы c циклами                                   *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнил: Мараков Андрей Дмитриевич | ИБКСб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach удалить все буквы и знаки препинания, оставить " +
+            "цифру затем преобразовать в число в строке: ?sd!! 5gh. s!");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение x: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.Calculate(x,y);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
