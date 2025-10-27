88d19475e871156d2ee98df239a4a74cefd27eaa
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-10-27 17:35:31 +0500

    Task1

---
+++ Tyuiu.MarakovAD.Sprint3.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.MarakovAD.Sprint3.Task1.V4.Lib
+{
+    public class DataService : ISprint3Task1V4
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double s = 0;
+            int k = startValue;
+            while (k <= stopValue) {
+                s = s + Math.Pow((1.0/k),2);
+                k++;
+            }
+            return Math.Round(s,3);
+        }
+    }
+}
---
+++ Tyuiu.MarakovAD.Sprint3.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MarakovAD.Sprint3.Task1.V4.Lib;
+namespace Tyuiu.MarakovAD.Sprint3.Task1.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.GetSumSeries(1, 3);
+            double wait = 1.361;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
