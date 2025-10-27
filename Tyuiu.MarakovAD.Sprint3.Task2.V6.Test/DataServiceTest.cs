436281fd777d8a8df9cf4f2516e9fc20289689cd
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-10-27 18:23:12 +0500

    Task2

---
+++ Tyuiu.MarakovAD.Sprint3.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MarakovAD.Sprint3.Task2.V6.Lib;
+namespace Tyuiu.MarakovAD.Sprint3.Task2.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest 
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.GetMultiplySeries(0.25,1, 3);
+            double wait = -0.377;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MarakovAD.Sprint3.Task2.V6/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.MarakovAD.Sprint3.Task2.V6.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Мараков А. Д. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы c циклами                                   *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Выполнил: Мараков Андрей Дмитриевич | ИБКСб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет произведение " +
+            "ряда по формуле");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение a: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите начальное значение: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите конечное значение: ");
+        int z = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.GetMultiplySeries(x, y, z);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
